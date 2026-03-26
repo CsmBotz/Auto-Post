d1da0d1de8280795b4e9195b3218291e1e5c8491
Yahiko / <yahiko5679@gmail.com>
2026-03-26 19:26:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
 import sys
+import asyncio
+
 _ROOT = os.path.dirname(os.path.abspath(__file__))
 os.chdir(_ROOT)
 sys.path.insert(0, _ROOT)
 
 from bot import main
 
-main()
+if __name__ == "__main__":
+    asyncio.run(main())
